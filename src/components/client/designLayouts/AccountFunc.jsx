025939afb89960210bfe93b982bcd68b2d0897ea
What57-ph <0plmzxc@gmail.com>
2025-04-28 00:56:26 +0700

    Update AccountFunc.jsx

--- src/components/client/designLayouts/AccountFunc.jsx
+++ src/components/client/designLayouts/AccountFunc.jsx
@@ -45,7 +45,7 @@ const AccountFunc = () => {
         <div className="flex-shrink-0">
           <img
             className="h-8 w-8 rounded-full object-cover"
-            src={currentUser.avatar}
+            src={currentUser?.avatar}
             alt="User avatar"
             width={40}
             height={40}
